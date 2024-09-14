d2e78032788751d618745121935c75c3ffadf9b1
Serhii Zaika <ewentualnie.sz@gmail.com>
2024-09-14 16:57:08 +0300

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { dataSourceOptionst } from './database/db-config';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(dataSourceOptionst),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
