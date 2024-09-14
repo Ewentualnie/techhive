e3ac76e1ead8ab6c0e5b203d217038356dd73cd3
Serhii Zaika <ewentualnie.sz@gmail.com>
2024-09-14 16:58:09 +0300

    create user entity * for test

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  firstName?: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  lastName?: string;
+}
