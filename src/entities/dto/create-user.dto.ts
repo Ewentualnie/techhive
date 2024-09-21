6307f1dbb19620289b3e35e9d5a5b7ef8f9e974d
Serhii Zaika <ewentualnie.sz@gmail.com>
2024-09-21 22:18:15 +0300

    add validation

--- src/entities/dto/create-user.dto.ts
+++ src/entities/dto/create-user.dto.ts
@@ -1,13 +1,32 @@
-import { IsNotEmpty, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsEmail, IsString } from 'class-validator';
+import { IsValidEmail } from 'src/validators/email-validator';
+import { IsValidPassword } from 'src/validators/pass-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'Email of the user',
+    example: 'user@example.com',
+  })
   @IsEmail()
   @IsNotEmpty()
+  @IsValidEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'Password of the user',
+    example: 'strongPassword123',
+  })
   @IsNotEmpty()
+  @IsString()
+  @IsValidPassword()
   pass: string;
 
+  @ApiProperty({
+    description: 'Confirmation of the password',
+    example: 'strongPassword123',
+  })
   @IsNotEmpty()
+  @IsString()
   pass2: string;
 }
