22a299fc283e370f620052668cc26f0184f4aaa5
Serhii Zaika <ewentualnie.sz@gmail.com>
2024-09-21 22:17:02 +0300

    add swagger api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,32 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { User } from 'src/entities/user.entity';
 import { CreateUserDto } from 'src/entities/dto/create-user.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'New user registration' })
+  @ApiResponse({
+    status: 201,
+    description: 'User information ',
+    content: {
+      'application/json': {
+        example: {
+          user: {
+            email: 'user@example.com',
+            id: 3,
+          },
+          // tokens: {
+          //   accessToken: 'accessToken',
+          //   refreshToken: 'refreshToken',
+          // },
+        },
+      },
+    },
+  })
   async register(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.authService.createUser(createUserDto);
   }
